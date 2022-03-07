28dd990d4bfb9a135064f0aea49073921500b2f2
ntyaiqingr <tongyangni1228@gmail.com>
2022-03-07 21:12:33 +0800

    bitwise operation

--- algoJava/src/algoJavaDay01/Code01_PrintB.java
+++ algoJava/src/algoJavaDay01/Code01_PrintB.java
@@ -5,7 +5,7 @@ public class Code01_PrintB {
 
     public  static  void print(int num) {
         for (int i = 31; i >=0 ; i--) {
-            // 左移不带符号 右移有带符号的
+            // 左移不带符号 右移有带符号的 >> & >>>
             // 1往左移动i位
             // num << 1 == num * 2
             System.out.print((num & (1 << i))  == 0 ? "0" : "1");
@@ -15,11 +15,26 @@ public class Code01_PrintB {
 
     public static void main(String[] args) {
         // 32位
-        int num = 1123123;
+        int a = 5;
         // ~取反 位运算 |, ^, &
-        int b = ~num;
-
-        print(num);
+        int b = ~a;
+        int c = 11;
+        print(a);
         print(b);
+        print(c);
+        System.out.println("=================");
+        print(a | c); // 有1 就可以
+        print(a ^ c); // 完全不相同
+        print(a & c); // 都是1 才是1
+        System.out.println("-------------------------");
+        int num = Integer.MIN_VALUE;
+        print(num);
+        System.out.println("!!!!!!!!!!");
+        // 带符号（正负）右移 原本是负数 现在还是
+        print(num >> 1);
+        System.out.println("!!!!!!!!!!");
+        // 不带符号（正负）右移 原本是负数现在不是了
+        print(num >>> 1);
+        System.out.println(num >>>1);
     }
 }
